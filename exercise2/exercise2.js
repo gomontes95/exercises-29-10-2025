7e16a2e0cb9f03e01568b0737128abf8774a902d
Diego Montes <gomontes95@gmail.com>
2025-10-28 11:40:52 -0600

    exercise 2 Unique words identifier

--- exercise2/exercise2.js
+++ exercise2/exercise2.js
@@ -3,46 +3,20 @@ import { stdin as input, stdout as output } from 'node:process';
 
 const rl = readline.createInterface({ input, output });
 
-const primeNumberGenerator = async() => {
+async function uniqueWordsCounter() {
+  const str = await rl.question( 'Introduce text to count your unique words: ');
+  
+  function removePunctuationAndCAPS(text) {
+    const cleanedText = text.replace(/[^a-zA-Z0-9\s]/g, '');
+    return cleanedText.toLowerCase();
+  }
 
-    const numberOfPrimes = await rl.question('How many prime numbers do you want to generate? ')
+  const uniqueWordsResults = new Set( 
+    removePunctuationAndCAPS(str).split(" ") 
+  );
 
-    function generateFirstNPrimes(n) {
-      if (n < 1) return [];
-
-      
-      let limit = 30;
-      let primes = [];
-
-      while (primes.length < n) {
-        primes = sieveOfEratosthenes(limit);
-        limit *= 2;
-      }
-
-      return primes.slice(0, n);
-    }
-
-    function sieveOfEratosthenes(limit) {
-      const sieve = new Array(limit + 1).fill(true);
-      sieve[0] = sieve[1] = false;
-
-      for (let i = 2; i * i <= limit; i++) {
-        if (sieve[i]) {
-          for (let j = i * i; j <= limit; j += i) {
-            sieve[j] = false;
-          }
-        }
-      }
-
-      return sieve
-        .map((isPrime, i) => (isPrime ? i : null))
-        .filter(Boolean);
-    }
-
-    console.log(generateFirstNPrimes(Number(numberOfPrimes)));
-    
-    rl.close();
-
+  console.log(`This written has ${[...uniqueWordsResults].length} `);
+  rl.close();
 }
 
-primeNumberGenerator();
+uniqueWordsCounter();
