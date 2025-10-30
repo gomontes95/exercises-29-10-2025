b0bf9038b24d9e1a2bf4492fbc082f224b237cf2
Diego Montes <gomontes95@gmail.com>
2025-10-30 15:15:18 -0600

    exercise 2 Unique words identifier without any built in function

--- exercise2/exercise2.js
+++ exercise2/exercise2.js
@@ -1,22 +1,23 @@
-import readline from 'node:readline/promises';
-import { stdin as input, stdout as output } from 'node:process';
+// import readline from 'node:readline/promises';
+// import { stdin as input, stdout as output } from 'node:process';
 
-const rl = readline.createInterface({ input, output });
+// const rl = readline.createInterface({ input, output });
 
-async function uniqueWordsCounter() {
-  const str = await rl.question( 'Introduce text to count your unique words: ');
+// async function uniqueWordsCounter() {
+//   const str = await rl.question( 'Introduce text to count your unique words: ');
   
-  function removePunctuationAndCAPS(text) {
-    const cleanedText = text.replace(/[^a-zA-Z0-9\s]/g, '');
-    return cleanedText.toLowerCase();
-  }
+//   function removePunctuationAndCAPS(text) {
+//     const cleanedText = text.replace(/[^a-zA-Z0-9\s]/g, '');
+//     return cleanedText.toLowerCase();
+//   }
 
-  const uniqueWordsResults = new Set( 
-    removePunctuationAndCAPS(str).split(" ") 
-  );
+//   const uniqueWordsResults = new Set( 
+//     removePunctuationAndCAPS(str).split(" ") 
+//   );
 
-  console.log(`This written has ${[...uniqueWordsResults].length} `);
-  rl.close();
-}
+//   console.log(`This written has ${[...uniqueWordsResults].length} `);
+//   rl.close();
+// }
 
-uniqueWordsCounter();
+// uniqueWordsCounter();
+
